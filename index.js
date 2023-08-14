9aa3511bd2ac81beccf916d5cd3877dc56b5ca4f
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-08-14 15:38:40 +0300

    finely

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'GLTFLoader';
+import { OrbitControls } from 'OrbitControls';
 
 const scene = new THREE.Scene();
 const sunLight = new THREE.DirectionalLight(0xffffff, 4);
